b7148da098bcf672fced540996f46ac718fe5f79
DevPardx <dev@idesignsv.com>
2025-10-12 13:33:39 -0600

    feat(config): add bcrypt integration, CORS configuration, nodemailer setup, error handling, validation middleware, and utility functions for token generation

---
+++ src/config/cors.ts
@@ -0,0 +1,17 @@
+import { CorsOptions } from "cors";
+
+export const corsConfig: CorsOptions = {
+  origin: (origin, callback) => {
+    const whitelist = [process.env.FRONTEND_URL];
+
+    if(process.env.NODE_ENV === "development") {
+      whitelist.push(undefined);
+    }
+
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
