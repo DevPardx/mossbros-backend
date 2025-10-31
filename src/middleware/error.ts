8ba743c8b809a72af55f7e77eff5aa8d6f65f9d6
DevPardx <dev@idesignsv.com>
2025-10-30 20:21:03 -0600

    fix: resolve eslint warnings

--- src/middleware/error.ts
+++ src/middleware/error.ts
@@ -1,7 +1,8 @@
 import { Request, Response, NextFunction } from "express";
 import { AppError } from "../handler/error.handler";
 
-export const errorHandler = ( err: Error, _req: Request, res: Response, _next: NextFunction ) => {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const errorHandler = ( err: Error, _req: Request, res: Response, _next: NextFunction ) => { 
     if (err instanceof AppError) {
         return res.status(err.statusCode).json({
             error: err.message,
